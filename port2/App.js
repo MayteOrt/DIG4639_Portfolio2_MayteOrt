31b1d7f9f938018f040a56e5a5cc66f410dd309f
MayteOrt <97573381+MayteOrt@users.noreply.github.com>
2022-04-21 19:58:41 -0400

    changes

--- port2/App.js
+++ port2/App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { createContext, useCallback, useImperativeHandle } from 'react';
+import { useCallback } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const Stack = creatNativeStackNavigator();
@@ -17,13 +17,28 @@ function legExerciseScreen({ route, navigation }) {
   )
 }
 
-
-export default function App() {
+function HomeScreen({ navigation }) {
+  let gotolegExercise = useCallback(() => {
+    navigation.navigate("LegExercise", {name: "Skull Crusher"})
+  })
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button onPress={gotoExercise} title="Go to Screen"></Button>
       <StatusBar style="auto" />
     </View>
+  )
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+       <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="LegExercise" component={legExerciseScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
