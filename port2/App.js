237c6fe5f53c4b1710b73d44f278a080a81b6bb4
MayteOrt <97573381+MayteOrt@users.noreply.github.com>
2022-04-23 18:22:50 -0400

    changes to readme

--- port2/App.js
+++ port2/App.js
@@ -3,15 +3,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
+
 const Stack = createNativeStackNavigator();
 
 function LegExerciseScreen({ navigation }) {
   return(
     <View style={styles.container}>
-      <Text>Leg Exercises <br></br> Let's get started!</Text>
+      <Text  >Leg Exercises <br></br> Let's get started!</Text>
       <Text>Elevated Goblet Squats 3x10</Text>
       <Text>Sumo Squats 3x10</Text>
-      <Text>Leg Press 3x10</Text>
       <Button title="Go to Home" onPress={() => navigation.navigate('Home')}></Button>
     </View>
   );
@@ -33,11 +33,11 @@ function ArmExerciseScreen({ navigation }) {
 function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Welcome to your personal fitness tracker! <br></br> Choose the desired area of focus.</Text>
-      <Button title="Leg Exercise" onPress={()=> navigation.navigate('LegExercise')} ></Button>
-     
-      <Button title="Arm Exercise" onPress={()=> navigation.navigate('ArmExercise')} ></Button>
-      
+
+      <Text >Welcome to your personal fitness tracker! </Text>
+      <Text>Choose the desired area of focus.</Text>
+      <Button title="Leg Exercises" onPress={()=> navigation.navigate('LegExercise')} ></Button>
+      <Button title="Arm Exercises" onPress={()=> navigation.navigate('ArmExercise')} ></Button>
       <StatusBar style="auto" />
     </View>
   )
@@ -56,27 +56,25 @@ export default function App() {
   );
 }
 
-
 const styles = StyleSheet.create({
-  Button: {
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
     paddingVertical: 12,
-    paddingHorizontal: 32,
+    paddingHorizontal: 12,
     borderRadius: 4,
     elevation: 3,
   },
   container: {
-    flex: 1,
+    flex: 5,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30,
   },
   Text: {
     fontSize: 16,
     lineHeight: 21,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
+    alignContent: 'center',
 },
 });
